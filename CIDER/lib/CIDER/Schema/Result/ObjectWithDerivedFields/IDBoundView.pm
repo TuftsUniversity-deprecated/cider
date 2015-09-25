ee4c29ea3d61c44f3343b95ad4b9f934881b5c08
Jason McIntosh <jmac@jmac.org>
2015-09-25 11:18:28 -0400

    Improving date-range fetching SQL.

--- CIDER/lib/CIDER/Schema/Result/ObjectWithDerivedFields/IDBoundView.pm
+++ CIDER/lib/CIDER/Schema/Result/ObjectWithDerivedFields/IDBoundView.pm
@@ -9,7 +9,10 @@ __PACKAGE__->result_source_instance->is_virtual(1);
 __PACKAGE__->result_source_instance->view_definition("select
                   object.*,
                   min(item_date_from) as earliest,
-                  greatest( max(item_date_to), max(item_date_from) ) as latest,
+                  greatest(
+                  coalesce ( max(item_date_to), 0 ),
+                  coalesce ( max(item_date_from), 0 )
+                  ) as latest,
                   group_concat(distinct accession_number) as accession_numbers,
                   group_concat(distinct restrictions) as restrictions
                   from object, item, enclosure
